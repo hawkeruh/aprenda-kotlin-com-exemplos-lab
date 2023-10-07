250b0ce81e570eab82a0a6d5b857af9ec0e7b598
root <root@guts>
2023-10-07 01:45:43 -0300

    estrutura da aplicação completa.

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Desafio de Projeto criado para avaliação do conteúdo técnico explorado no re
 ```kotlin
 TODO("Crie uma solução em Koltin abstraindo esse domínio. O arquivo [desafio.kt] te ajudará 😉")
 ```
+
+# Resposta do Desafio
+
+Para a aplicação foi feito os objetos: 'Usuario', 'Professor', 'ConteudoEducacional' e 'Formacao'.
+
+Para os objetos de classe de dados foi utilizado variavéis imutavéis por questões de segurança. 
+
+Foi montado três cenários diferentes para demonstrar o uso dos objetos com seus atributos e funções. As informações (input) a serem passadas para os objetos foram colocadas diretamente no main para simular os cenários ao invés de perguntas ao usuários utilizando funções (tipo 'cadastrarProfessor()') para focar na orientação de objetos utilizando Kotlin. 
+
+Existem vários modos de otimizar esse código quando se trata da criação de objetos e a inserção dos mesmos dentro da lista, evitando repetições. Porém, desse modo, está legível e dá pra entender passo a passo a lógica para seguir nos cenários, está mirado em um código mais educacional. 
+
+Já que todos os input são feitos dentro do main, capturar exceções de erros de input são desnecessários.
+
+## Cenário 1 
+
+Há uma mudança no elenco de professores da Dio. O professor Gustavo está se aposentando e um novo professor foi contratado! O novo professor chamado Vlad Tepes inicia lecionando o conteudo de 'Teoria dos Grafos' com os demais professores.
+
+## Cenário 2 
+
+Para aproveitar a sua especialidade em computação quântica e interesse do mercado na área, professor Vlad Tepes cria um conteudo detalhado e avançado sobre o assunto. A formação é de nível avançado e especializado.
+
+## Cenário 3
+
+Grande quantidade de novatos se inscrevem devido ao sucesso do conteúdo abrangendo a computação quântica. Surgindo assim uma nova necessidade: Uma formação para ensinar os fundamentos necessários para um melhor entendimento e facilidade para acompanhar o curso avançado de computação quântica. 
+Nova leva de professores são contratados, e pela primeira vez na plataforma, não são professores da área de tecnologia, mas da área de física, matemática e química. 
---
+++ resposta.kt
@@ -0,0 +1,59 @@
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+
+class Usuario (val nome: String, val id: Int, var email: String)
+
+class Professor (val nome: String, val id: Int, var horas: Int) {
+    override fun toString(): String {
+        return "Prof. $nome"
+    }
+}
+
+data class ConteudoEducacional(val nome: String, val duracao: Int, val profs: List<Professor>)
+
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Usuario>()
+    
+    fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+    }
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+    }
+}
+
+fun main() {
+
+    //lista de professores inicial e uma formação com conteudo de dois módulos(conteudoeducacional)
+    val professorLista = mutableListOf(Professor("Gustavo", 177, 32), Professor("Cássia", 512, 29), Professor("Marlon", 205, 12))
+    val conteudoLista =  mutableListOf(ConteudoEducacional("Python", 15, professorLista), ConteudoEducacional("Algoritmos", 20, professorLista))
+    val ed1 = Formacao("Estrutura de Dados", Nivel.BASICO, conteudoLista)
+    println(ed1)
+
+    //CENÁRIO 1
+    professorLista.remove(Professor("Gustavo", 177, 32))
+    val dracula = Professor("Vlad Tepes", 66, 0)
+    professorLista.add(dracula)
+    //Atualizar horas dos professores usando find id
+    val conteudo2 = mutableListOf(ConteudoEducacional("Teoria dos Grafos", 30, professorLista))
+    val neural = Formacao("Redes Neurais", Nivel.INTERMEDIARIO, conteudo2)
+    println(neural)
+    
+    //CENÁRIO 2
+    val profDracula = mutableListOf(dracula)
+    val conteudo_especializado = mutableListOf(ConteudoEducacional("Circuitos Quânticos", 19, profDracula), ConteudoEducacional("Mecânica Quântica", 30, profDracula), ConteudoEducacional("Teoria da Complexidade Quântica", 13, profDracula))
+    val curso_avancado = Formacao("Computação Quântica", Nivel.AVANCADO, conteudo_especializado)
+    println(curso_avancado)
+
+    //CENÁRIO 3 
+    curso_avancado.matricular(Usuario("Hercules Martins Silva",10032,"hercu_poirot@myemail.com"))
+    curso_avancado.matricular(Usuario("Diego Tranquinas",10044,"trakinasbiscuit@myemail.com"))
+    curso_avancado.matricular(Usuario("Simone Cardoso",100682,"fhc_filha@myemail.com"))
+
+    val professorAREA2 = mutableListOf(Professor("Vanice", 300, 0), Professor("Tulio", 301, 0), Professor("Geraldo", 302, 0), dracula)
+    val conteudo_mathplus = mutableListOf(ConteudoEducacional("Física", 30, professorAREA2), ConteudoEducacional("Cálculo", 60, professorAREA2), ConteudoEducacional("Algebra Linear", 15, professorAREA2),)
+    val curso_fundamentos = Formacao("Fundamentos", Nivel.BASICO, conteudo_mathplus)
+
+    println(curso_fundamentos)
+
+}
